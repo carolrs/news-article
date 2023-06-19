d979f2a4d1014c51e8bbf82c900e17693c8a0ba6
ana carolina <anacarolinarodriguesoares@gmail.com>
2023-06-19 16:01:03 +0100

    Footer for the site

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-black text-center p-4 mt-10">
+      <p>&copy; {new Date().getFullYear()} NY Search App</p>
+        </footer>
+  );
+};
+
+export default Footer;
